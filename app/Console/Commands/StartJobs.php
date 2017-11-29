c6954035e5c78474eda6bf324ccb5c8110adcaf7
William Klausmeyer <willyk95@gmail.com>
2017-11-29 10:26:31 -0600

    Add a command to start jobs

---
+++ app/Console/Commands/StartJobs.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StartJobs extends Command
+{
+    protected $signature = 'crawl:start_jobs {count : The number of jobs to start}';
+
+    protected $description = 'Start any number of blank jobs';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle($count)
+    {
+        // Dispatch jobs
+        for ($i = 0; $i < $count; $i++) {
+            app('Illuminate\Contracts\Bus\Dispatcher')->dispatch(new PageFetcher());
+        }
+    }
+}
